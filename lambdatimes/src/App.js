ea537012f78b9dee19c2d166d48173ecf700f15a
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-02-22 10:55:02 +0100

    Refactored all styling in TopBar into separate styled-components in './TopBar'

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import TopBar from './components/TopBar';
 import Header from './components/Header';
 import Content from './components/Content/Content';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  margin: 0;
+  padding: 0;
+  background-color: #f1f1f1;
+  color: #333;
+  font-family: 'PT Sans', sans-serif;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: #f1f1f1;
+  color: #333;
+  font-family: 'PT Sans', sans-serif;
+`;
 
 const App = () => {
   return (
-    <div className="App">
+    <StyledApp className="App">
       <TopBar />
       <Header />
       <Content />
-    </div>
+    </StyledApp>
   );
 }
 
